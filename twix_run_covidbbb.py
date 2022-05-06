a7e6fc222a6667173a9313b8cd89ea3177fc9cbe
sapje1 <evansj31@cardiff.ac.uk>
2022-05-06 15:08:05 +0100

    additional data for bbbcov

--- twix_run_covidbbb.py
+++ twix_run_covidbbb.py
@@ -18,7 +18,8 @@ data_dir = '/home/sapje1/data_sapje1/projects/bbbcovid'
 in_file = [ '220315_pilot/meas_MID165_sLaser_Brainstem_20_12_12_FID118913.dat',\
             '220401_pilot/meas_MID199_sLaser_Brainstem_20_12_12_FID120393.dat',\
              'oxford017/meas_MID156_sLaser_Brainstem_20_12_12_FID7821.dat',\
-             'oxford018/meas_MID112_sLaser_Brainstem_20_12_12_FID8065.dat' ]
+             'oxford018/meas_MID112_sLaser_Brainstem_20_12_12_FID8065.dat',
+             'CAU002N01/meas_MID68_sLaser_Brainstem_20_12_12_FID121048.dat' ]
 #in_file = ['220315_pilot/meas_MID165_sLaser_Brainstem_20_12_12_FID118913.dat']
 
 
@@ -34,10 +35,11 @@ mrs_data = twixmrs.twixmrs_load_basic(data_dir, in_file)
 mrs_data_phased = []
 mrs_data_phased.append( mrs_data[0].adjust_phase(-0.4,0.0015) )  #cubric_220315
 mrs_data_phased.append( mrs_data[1].adjust_phase(-0.4,0.0015) )  #cubric_220315
+mrs_data_phased.append( mrs_data[4].adjust_phase(-0.4,0.0015) )  # CAU002N01
 mrs_data_phased.append( mrs_data[2].adjust_phase(-0.5,0.002))   # oxford 017
 mrs_data_phased.append( mrs_data[3].adjust_phase(-0.5,0.002))   # oxford 018
 
-line_label = ['CUBRIC_220315', 'CUBRIC_220401', 'Oxford_017','Oxford_018']
+line_label = ['CUBRIC_220315', 'CUBRIC_220401', 'CAU002N001', 'Oxford_017','Oxford_018']
 twixmrs.twixmrs_plot(mrs_data_phased, line_label)
 
 
